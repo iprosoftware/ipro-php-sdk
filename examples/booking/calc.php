edad849afb67ebf5ec53e956bd7cc661b2c9454d
Luke Lincoln <luke@ipro-software.com>
2017-01-24 12:02:13 +0100

    Updated with comments

--- examples/booking/calc.php
+++ examples/booking/calc.php
@@ -7,27 +7,31 @@ $client = new IProClient('1000', '0bd58c64591b407e969c72ffaffeaf99', 'http://loc
 $resp = $client->getAccessToken();
 
 if ($resp['code'] == 200) {
-    
+
     $result = $resp['result'];
     $client->setAccessToken($result['access_token']);
-    
+
+    //Get the property list
+    //Under normal circumstances this wouldn't be necessary
+    //But demonstrates the functionality
     $resp = $client->propertyList();
 
-    if(count($resp['result']) > 0){
-        
+    if (count($resp['result']) > 0) {
+
+        //Choose a property at random for the demonstration
         $property_index = array_rand($resp['result']);
         $property_id = $resp['result'][$property_index]['Id'];
+        
+        //Choose sensible dates for the demonstration
         $start_date = new DateTime('next saturday');
-
         $end_date = new DateTime();
         $end_date->setTimestamp($start_date->getTimestamp());
         $end_date->add(new DateInterval('P7D'));
 
         $calculated_booking = $client->calculateBooking($property_id, $start_date->format('Y-m-d'), $end_date->format('Y-m-d'), 2, 1, 0);
-        
+
         echo json_encode($calculated_booking);
     }
-    
 } else {
     var_dump($resp['result']);
 }
